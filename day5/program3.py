7207ab4acb00c858e8b080f3b202d42910382238
Shivam Kumar <43021139+ShivamKumar-sk@users.noreply.github.com>
2020-06-03 10:56:25 +0530

    Update program3.py

--- day5/program3.py
+++ day5/program3.py
@@ -1,12 +1,17 @@
-c=0
-def Stole(x):
+
+def Stole(x,c):
     if c%2==0:
         max_value=sorted(x)[-1]
-    return max_value
+        return max_value
+    else:
+        return 0
         
 t=int(input('enter number of houses:'))
 Lst=[]
+c=0
 for i in range(t):
     x=[int(x) for x in (input('enter values in houses:').split())]
-    Lst.append(Stole(x))
+    k=Stole(x,c)
+    c+=1
+    Lst.append(k)
 print('maximum stolen value is:',sorted(Lst)[-1])
