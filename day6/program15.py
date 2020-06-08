dc1c1229a043be0cb753e76b099167b55ffcebcf
Shivam Kumar <43021139+ShivamKumar-sk@users.noreply.github.com>
2020-06-08 15:50:45 +0530

    Update program15.py

--- day6/program15.py
+++ day6/program15.py
@@ -1,35 +1,18 @@
-
-def partition3(a, l, r):
-    x = a[l]
-    j = l
-    for i in range(l+1, r+1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-
+arr=[int(arr) for arr in input('enter values:').split()]
+s=0
+k=0
+res=0
+for i in range(len(arr)):
+    x=i
+    s=arr[0:i]
+    k=arr[i+1:]
+    s=sorted(s)
+    k=sorted(k)
+    if len(s)>0 and len(k)>0:
+        if s[-1]<=arr[x] and k[0]>=arr[x]:
+            res=x
+            print(arr[x])
+            break
     
-
-def randomized_quick_sort(a, l, r):
-    if l >= r:
-        return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    m = partition3(a, l, r)
-   
-    randomized_quick_sort(a, l, m-1);
-    randomized_quick_sort(a, m+1, r);
     
-
-
-
-if __name__ == '__main__':
-    n=int(input())
-    a=[int(a) for a in input().split()]
-    x=a
-    if len(a)==n:
-        randomized_quick_sort(a, 0, len(a)-1)
-    k=a[len(a)//2]
-    print('element is', x[k])
         
