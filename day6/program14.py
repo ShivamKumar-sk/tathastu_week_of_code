4999ebe151ae9a235bf2f56ffda3bec2e6a94282
Shivam Kumar <43021139+ShivamKumar-sk@users.noreply.github.com>
2020-06-08 19:06:53 +0530

    Update program14.py

--- day6/program14.py
+++ day6/program14.py
@@ -27,9 +27,9 @@ mat = [[0 for x in range(n)] for y in range(n)]
 
 
 mat = [ [1, 2, 3, 4 ], 
-		[5, 6, 7, 8 ], 
-		[9, 10, 11, 12 ], 
-		[13, 14, 15, 16 ] ] 
+	[5, 6, 7, 8 ], 
+	[9, 10, 11, 12 ], 
+	[13, 14, 15, 16 ] ] 
 		
 rotate(mat) 
 displayMatrix(mat) 
